81afd99d10381b1ad8a36742a882b661b7dc1b0a
Ryo Hashiguchi <ryo12redstone@gmail.com>
2017-04-30 02:24:48 +0900

    0.1.2

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PhoenixChannelClient.Mixfile do
     [app: :phoenixchannelclient,
      description: "Phoenix Channel Client",
      package: package(),
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
